cb31dde8b87e948c540a34872d4bf0a8ebd66920
lin-xin <2981207131@qq.com>
2017-05-05 17:59:06 +0800

    Add cart

--- page/component/cart/cart.js
+++ page/component/cart/cart.js
@@ -1,19 +1,122 @@
 // page/component/new-pages/cart/cart.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    carts:[],
+    hasList:true,
+    totalPrice:0,
+    selectAllStatus:true
   },
-  onReady:function(){
-    // 页面渲染完成
+  onLoad() {
+    this.getTotalPrice();
   },
-  onShow:function(){
-    // 页面显示
+  onShow() {
+    this.setData({
+      hasList: true,
+      carts:[
+        {id:1,title:'新鲜芹菜 半斤',image:'/image/s5.png',num:4,price:0.01,selected:true},
+        {id:2,title:'素米 500g',image:'/image/s6.png',num:1,price:0.03,selected:true}
+      ]
+    });
   },
-  onHide:function(){
-    // 页面隐藏
+  /**
+   * 当前商品选中事件
+   */
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    const selected = carts[index].selected;
+    carts[index].selected = !selected;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
   },
-  onUnload:function(){
-    // 页面关闭
+
+  /**
+   * 删除购物车当前商品
+   */
+  deleteList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index,1);
+    this.setData({
+      carts: carts
+    });
+    if(!carts.length){
+      this.setData({
+        hasList: false
+      });
+    }else{
+      this.getTotalPrice();
+    }
+  },
+
+  /**
+   * 购物车全选事件
+   */
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus;
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
+
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus;
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    });
+    this.getTotalPrice();
+  },
+
+  /**
+   * 绑定加数量事件
+   */
+  addCount(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num = num + 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
+  },
+
+  /**
+   * 绑定减数量事件
+   */
+  minusCount(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    if(num <= 1){
+      return false;
+    }
+    num = num - 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
+  },
+
+  /**
+   * 计算总价
+   */
+  getTotalPrice() {
+    let carts = this.data.carts;
+    let total = 0;
+    for(let i = 0; i<carts.length; i++) {
+      if(carts[i].selected) {
+        total += carts[i].num * carts[i].price;
+      }
+    }
+    this.setData({
+      carts: carts,
+      totalPrice: total.toFixed(2)
+    });
   }
+
 })
