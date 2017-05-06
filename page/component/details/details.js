01c355906ba348e4756a292a0bc9b8e3f781a9de
lin-xin <2981207131@qq.com>
2017-05-06 18:03:19 +0800

    Add goods details

---
+++ page/component/details/details.js
@@ -0,0 +1,18 @@
+// page/component/details/details.js
+Page({
+  data:{
+    goods: {
+      id: 1,
+      image: '/image/goods1.png',
+      title: '新鲜梨花带雨',
+      price: 0.01,
+      stock: '有货',
+      detail: '这里是梨花带雨详情。',
+      parameter: '125g/个',
+      service: '不支持退货'
+    },
+    num: 1,
+    hasCarts: false
+  },
+ 
+})
