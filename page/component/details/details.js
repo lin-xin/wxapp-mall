aa213f8059b23197fe29a740bdabdcc77e32d467
lin-xin <2981207131@qq.com>
2017-05-08 09:58:18 +0800

    Add details

--- page/component/details/details.js
+++ page/component/details/details.js
@@ -12,7 +12,50 @@ Page({
       service: '不支持退货'
     },
     num: 1,
-    hasCarts: false
+    totalNum: 0,
+    hasCarts: false,
+    curIndex: 0,
+    show: false,
+    scaleCart: false
   },
+
+  addCount() {
+    let num = this.data.num;
+    num++;
+    this.setData({
+      num : num
+    })
+  },
+
+  addToCart() {
+    const self = this;
+    const num = this.data.num;
+    let total = this.data.totalNum;
+
+    self.setData({
+      show: true
+    })
+    setTimeout( function() {
+      self.setData({
+        show: false,
+        scaleCart : true
+      })
+      setTimeout( function() {
+        self.setData({
+          scaleCart: false,
+          hasCarts : true,
+          totalNum: num + total
+        })
+      }, 200)
+    }, 300)
+
+  },
+
+  bindTap(e) {
+    const index = parseInt(e.currentTarget.dataset.index);
+    this.setData({
+      curIndex: index
+    })
+  }
  
 })
